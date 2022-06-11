57a4ab283fae40568247233b3488d9821ff08eb7
smndhm <simon.duhem@gmail.com>
2022-06-11 12:45:30 +0200

    feat: add ids on matchs

--- src/views/MatchsView.vue
+++ src/views/MatchsView.vue
@@ -1,13 +1,14 @@
 <script setup>
 import MatchDetails from "../components/MatchDetails.vue";
 import { getMatchsByHours } from "../services/sheets.service";
+import { paramCase } from "param-case";
 const matchHours = await getMatchsByHours();
 </script>
 
 <template>
   <h2>Matchs</h2>
   <template v-for="matchHour in matchHours" :key="matchHour.heure">
-    <h3>
+    <h3 :id="paramCase(matchHour.heure)">
       {{ matchHour.heure }}
     </h3>
     <div class="columns">
